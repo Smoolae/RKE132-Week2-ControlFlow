5299e45eb4f94936822c65620cc65cbac490f02d
Samuli Martin <martinsamuli@gmail.com>
2023-02-25 16:50:24 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutaja soo (m/f)
+//rakendus küsib kasutajalt tema perekonnanime
+//lähtudes valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi]" / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+
+Console.WriteLine("PLease select your gender (m/f)");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb andmeid string (sõne) formaadis
+
+Console.WriteLine("Please enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+
+
---
+++ UserAgeVerification/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajalt sisetada enda vanuse
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+//"You are too young to use this program"
+//muul juhul:
+//"Welcome to program"
+
+Console.WriteLine("Please enter your age:");
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean - true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if(isAgeNumber)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to program!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use this program");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age. Enter your age using numbers");
+}
+
+
+
+
+
+
+
+
+
+//int userAge = Int32.Parse(Console.ReadLine()); //"13" - heap, 13 - stack
+
+//if (userAge >= 13)
+//{
+    //Console.WriteLine("Welcome to program!");
+//}
+//else //if (userAge < 13)
+//{
+    //Console.WriteLine("You are too young to use this program");
+//}
+
+
